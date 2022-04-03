eb9d5bc635a63c60f1caf85db3acc51d35eef2d6
BPRJon <65429548+BPRJon@users.noreply.github.com>
2022-04-03 00:37:35 -0400

    Add files via upload

---
+++ ARC-9-Airsoft/lua/arc9/common/attachments/ammo_bb.lua
@@ -0,0 +1,17 @@
+ATT.PrintName = "Plastic BBs"
+ATT.CompactName = "BB"
+ATT.Icon = Material("entities/arc9_att_ammo.png", "mips smooth")
+ATT.Description = [[Bullets made of plastic, typically used in the sport "Airsoft".]]
+ATT.SortOrder = 0
+
+ATT.Category = {"ammo"}
+
+ATT.SpreadMult = 1
+ATT.PhysBulletMuzzleVelocityMult = 1
+
+ATT.DamageMaxMult = -10
+ATT.DamageMinMult = -10
+
+ATT.PenetrationMult = 0
+ATT.RicochetChanceMult = 1.45
+ATT.ArmorPiercingAdd = 0
